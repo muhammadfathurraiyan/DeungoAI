af0dc3466d5d087e9788712d262e7474c3020809
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-09-15 14:57:05 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_VERCEL_ENV': JSON.stringify(process.env.VITE_VERCEL_ENV)
+  }
 })
